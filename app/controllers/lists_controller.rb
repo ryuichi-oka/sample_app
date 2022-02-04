5a720f17fde729c1baeccea90962cda6c404d37a
EC2 Default User <ec2-user@ip-172-31-42-83.ec2.internal>
2022-02-04 16:39:16 +0000

    [finish]3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,8 +9,8 @@ class ListsController < ApplicationController
     list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    # 4. 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -19,14 +19,29 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+  # showアクションにリダイレクトさせるため、必ずidを入れる
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists' #投稿一覧画面へリダイレクト
   end
 
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
